401b571ac997e587b93c57041ff55ee3f18929af
LeoSan <cuenca623@gmail.com>
2021-05-20 17:49:32 -0500

    Autor: Leonard, Descripción, Se aprendio a realizar una consulta de la suma de todos los patimonios de un usuario filtrado por fecha, se uso el metodo aggregate en conjunto con el group match order

--- servidor/routes/patrimonio.js
+++ servidor/routes/patrimonio.js
@@ -43,6 +43,18 @@ router.get('/patrimoniobyfecha',
       patrimonioCotroller.getPatrimonioByFecha
 );
 
+//End-Point - Consultar Patrimonio  por fecha 
+router.get('/patrimonioSumabyfecha',
+      auth,  
+      [
+      check('fechaConsultar', 'El campo fecha es obligatorio.').not().isEmpty(), //Valida vacio
+      check('usuario',        'El campo usuario es obligatorio.').not().isEmpty(), //Valida vacio
+      check('categoria',      'El campo categoria es obligatorio.').not().isEmpty(), //Valida vacio
+      check('activo',         'El campo activo es obligatorio.').not().isEmpty(), //Valida vacio
+      ] ,      
+      patrimonioCotroller.getPatrimonioSumaByFecha
+);
+
 //End-Point - Editar Patrimonio
 router.put('/', 
       auth,
